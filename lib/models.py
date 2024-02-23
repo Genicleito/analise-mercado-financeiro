e3914ed553aca9ebac1e665f86807d799c51c994
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2024-02-23 02:13:21 -0300

    lib/models

---
+++ lib/models.py
@@ -0,0 +1,5 @@
+URL_LIB = 'https://raw.githubusercontent.com/Genicleito/market_trading_analysis/master/lib/technical_analysis/__init__.py'
+
+# Paths
+LIB_PATH = 'lib/technical_analysis.py'
+READ_MARKET_DATA_PATH = 'data/raw/hist_market_trading_yfinance.csv'
