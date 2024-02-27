9679fe37fee73506c57e3d6d6b71573de4df9a5b
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2024-02-27 18:24:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ if ticker_sb:
     st.dataframe(
         df[
             (df['tend_alta_medias'] == True) & (df['date'] == df['date'].max())
-        ].sort_values('date', ascending=False).reset_index(drop=False),
+        ].sort_values('date', ascending=False).reset_index(drop=True),
         width=900
     )
 
