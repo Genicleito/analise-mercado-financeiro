e013d23db62268acdb9195b1985ab33244885dd9
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2024-03-04 18:36:54 -0300

    Update models.py

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,8 @@
 URL_LIB = 'https://raw.githubusercontent.com/Genicleito/market_trading_analysis/master/lib/technical_analysis/__init__.py'
 
 # Paths
-LIB_PATH = 'lib/technical_analysis.py'
+LIB_PATH = 'lib'
+TECHNICAL_ANALYSIS_MODULE_PATH = f'{LIB_PATH}/technical_analysis.py'
 READ_MARKET_DATA_PATH = 'data/raw/hist_market_trading_yfinance.csv.zip'
 
 # Constantes
